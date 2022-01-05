db0962e2e2860a20e6d32e01d0fc24b8d0c7bfa6
Max <misterzetiu123@mail.ru>
2022-01-05 18:46:35 +0300

    dvizhok
    bugfix weapon 2 and weapon 1

--- main.py
+++ main.py
@@ -376,6 +376,10 @@ class Gameboard(Board):
                 self.butweap1active = False
             else:
                 self.butweap1active = True
+            for i in range(len(self.boardshow)):
+                for j in range(len(self.boardshow[i])):
+                    print(self.boardshow[i][j], end='')
+                print()
 
         elif mouse_pos[0] in range(
                 self.left + self.butweap2cord,
@@ -389,6 +393,10 @@ class Gameboard(Board):
                 self.butweap2active = False
             else:
                 self.butweap2active = True
+            for i in range(len(self.boardshow)):
+                for j in range(len(self.boardshow[i])):
+                    print(self.boardshow[i][j], end='')
+                print()
 
 
 
@@ -409,22 +417,15 @@ class Gameboard(Board):
                 print(self.hero.pos)
                 for i in range(self.height):
                     for j in range(self.width):
-                        if self.boardshow[i][j] == 'e1' or self.boardshow[i][j] == 'e12':
-                            print(j, i, end='<===== \n')
-                            print()
-                            if abs(self.playery - i) > self.hero.range or abs(self.playerx - j) > self.hero.range:
-                                self.boardshow[i][j] = 'e'
-
-                        if self.boardshow[i][j] == 'e' or self.boardshow[i][j] == 'e2':
-                            if abs(self.playery - i) <= self.hero.range and abs(self.playerx - j) <= self.hero.range:
-                                self.boardshow[i][j] = 'e1'
+                        if self.boardshow[i][j] == 'e1' or self.boardshow[i][j] == 'e12' or \
+                                self.boardshow[i][j] == 'e3' or self.boardshow[i][j] == 'e32':
+                            self.boardshow[i][j] = 'e'
 
                         if self.boardshow[i][j] == 'e' or self.boardshow[i][j] == 'e2':
                             if abs(self.playery - i) <= self.weap2range and abs(self.playerx - j) <= self.weap2range:
                                 self.boardshow[i][j] = 'e3'
                             if abs(self.playery - i) <= self.hero.range and abs(self.playerx - j) <= self.hero.range:
                                 self.boardshow[i][j] = 'e1'
-
                 for i in range(len(self.boardshow)):
                     for j in range(len(self.boardshow[i])):
                         print(self.boardshow[i][j], end='')
